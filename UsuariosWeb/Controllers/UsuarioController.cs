bb67143e91a09185fd034c7c988b00fc1e55cc49
johnny9052 <alexander9052@gmail.com>
2016-02-24 19:40:52 -0500

    Commit inicial

---
+++ UsuariosWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace UsuariosWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private EjercicioFrameworkEntities db = new EjercicioFrameworkEntities();
+
+        // GET: /Usuario/
+        public ActionResult Index()
+        {
+            var usuario = db.Usuario.Include(u => u.Municipio);
+            return View(usuario.ToList());
+        }
+
+        // GET: /Usuario/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: /Usuario/Create
+        public ActionResult Create()
+        {
+            ViewBag.idMunicipio = new SelectList(db.Municipio, "id", "nombre");
+            return View();
+        }
+
+        // POST: /Usuario/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,idMunicipio,nombre,apellido,cedula,fecha_nacimiento,salario")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idMunicipio = new SelectList(db.Municipio, "id", "nombre", usuario.idMunicipio);
+            return View(usuario);
+        }
+
+        // GET: /Usuario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idMunicipio = new SelectList(db.Municipio, "id", "nombre", usuario.idMunicipio);
+            return View(usuario);
+        }
+
+        // POST: /Usuario/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,idMunicipio,nombre,apellido,cedula,fecha_nacimiento,salario")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idMunicipio = new SelectList(db.Municipio, "id", "nombre", usuario.idMunicipio);
+            return View(usuario);
+        }
+
+        // GET: /Usuario/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: /Usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
